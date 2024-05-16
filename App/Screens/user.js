f32c4a42a4495fdde7ab74ae22679a167ef9b9ed
asierradev05 <angel.sierra0507@gmail.com>
2024-05-15 21:05:13 -0500

    Screens commit
    add all screens that project

---
+++ App/Screens/user.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+} from "react-native";
+
+const User = () => {
+    <View>
+        <Text>
+        User
+        </Text>
+    </View>
+
+}
+export default User;
