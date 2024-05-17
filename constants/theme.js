eea42a4b02ce4b47b6e4b7787669f7859cfd6895
asierradev05 <angel.sierra0507@gmail.com>
2024-05-16 21:16:24 -0500

    updateConstants

--- constants/theme.js
+++ constants/theme.js
@@ -1,12 +1,2 @@
-import Home from "../App/Screens/home";
-import Cryptos from "../App/Screens/cryptos";
-import index from "../App/Screens";
-import inversions from "../App/Screens/inversions";
-import user from "../App/Screens/user";
-export{
-    Home,
-    Cryptos,
-    index,
-    inversions,
-    user
-}
+import { Dimensions } from "react-native";
+const {whit, height}=Dimensions.get("window")
