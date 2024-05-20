e1f7d01920bb3712aa28ae3ef68c2288452fcf9d
asierradev05 <angel.sierra0507@gmail.com>
2024-05-19 22:29:40 -0500

    Update Screen Home for conect all screens

--- App/Screens/home.js
+++ App/Screens/home.js
@@ -1,16 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, Animated, Easing, StatusBar } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import { Ionicons } from '@expo/vector-icons';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  Animated,
+  Easing,
+  StatusBar,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const animation = useState(new Animated.Value(0))[0];
+  const navigation = useNavigation();
 
-  const handleLogin = () => {
-    console.log('Email:', email);
-    console.log('Contraseña:', password);
+  const navigateToRegister = () => {
+    navigation.navigate("RegisterIndex");
+  };
+  const InicioSesion = () => {
+    navigation.navigate("index");
   };
 
   useEffect(() => {
@@ -23,14 +37,24 @@ const LoginScreen = () => {
   }, [animation]);
 
   return (
-    <LinearGradient colors={['#1E2749', '#283C63']} style={styles.container}>
+    <LinearGradient colors={["#1E2749", "#283C63"]} style={styles.container}>
       <StatusBar barStyle="light-content" />
-      <Animated.View style={[styles.logoContainer, { transform: [{ scale: animation }] }]}>
-        <Image source={require('../../assets/images/LogoTrasparente.png')} style={styles.logo} />
+      <Animated.View
+        style={[styles.logoContainer, { transform: [{ scale: animation }] }]}
+      >
+        <Image
+          source={require("../../assets/images/LogoTrasparente.png")}
+          style={styles.logo}
+        />
       </Animated.View>
       <Text style={styles.title}>Welcome To EconomyAPP</Text>
       <View style={styles.inputContainer}>
-        <Ionicons name="mail-outline" size={24} color="#ccc" style={styles.icon} />
+        <Ionicons
+          name="mail-outline"
+          size={24}
+          color="#ccc"
+          style={styles.icon}
+        />
         <TextInput
           style={styles.input}
           placeholder="Email"
@@ -40,7 +64,12 @@ const LoginScreen = () => {
         />
       </View>
       <View style={styles.inputContainer}>
-        <Ionicons name="lock-closed-outline" size={24} color="#ccc" style={styles.icon} />
+        <Ionicons
+          name="lock-closed-outline"
+          size={24}
+          color="#ccc"
+          style={styles.icon}
+        />
         <TextInput
           style={styles.input}
           placeholder="Password"
@@ -50,13 +79,18 @@ const LoginScreen = () => {
           secureTextEntry={true}
         />
       </View>
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+      <TouchableOpacity style={styles.button} onPress={InicioSesion}>
         <Text style={styles.buttonText}>Iniciar sesión</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.link} onPress={() => console.log('Ir a la pantalla de recuperación de contraseña')}>
+      <TouchableOpacity
+        style={styles.link}
+        onPress={() =>
+          console.log("Ir a la pantalla de recuperación de contraseña")
+        }
+      >
         <Text style={styles.linkText}>¿Olvidaste tu contraseña?</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.link} onPress={() => console.log('Ir a la pantalla de registro')}>
+      <TouchableOpacity style={styles.link} onPress={navigateToRegister}>
         <Text style={styles.linkText}>Registrarse</Text>
       </TouchableOpacity>
     </LinearGradient>
@@ -66,8 +100,8 @@ const LoginScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     paddingHorizontal: 20,
   },
   logoContainer: {
@@ -79,51 +113,51 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 28,
-    color: '#fff',
+    color: "#fff",
     marginBottom: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.2)",
     borderRadius: 25,
     marginBottom: 15,
     paddingHorizontal: 15,
     paddingVertical: 10,
-    width: '100%',
+    width: "100%",
   },
   icon: {
     marginRight: 10,
   },
   input: {
     flex: 1,
-    color: '#fff',
+    color: "#fff",
     height: 40,
   },
   button: {
-    width: '100%',
-    backgroundColor: '#1E88E5',
+    width: "100%",
+    backgroundColor: "#1E88E5",
     borderRadius: 25,
     paddingVertical: 15,
-    alignItems: 'center',
+    alignItems: "center",
     marginBottom: 10,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.3,
     shadowRadius: 5,
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   link: {
     marginBottom: 10,
   },
   linkText: {
-    color: '#fff',
-    textDecorationLine: 'underline',
+    color: "#fff",
+    textDecorationLine: "underline",
   },
 });
 
